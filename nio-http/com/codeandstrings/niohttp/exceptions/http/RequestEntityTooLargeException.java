e39edbde334f6611d5335efa91ee3265baccdfd8
simplepanda <dylan@codeandstrings.com>
2014-08-11 09:11:16 -0400

    Updated to support passing of the proposed request size (from a content-length header) that triggered the exception.

--- nio-http/com/codeandstrings/niohttp/exceptions/http/RequestEntityTooLargeException.java
+++ nio-http/com/codeandstrings/niohttp/exceptions/http/RequestEntityTooLargeException.java
@@ -1,9 +1,22 @@
 package com.codeandstrings.niohttp.exceptions.http;
 
 public class RequestEntityTooLargeException extends HttpException {
-	private static final long serialVersionUID = 6676247325789548938L;
 	
-	public RequestEntityTooLargeException() {
+	private static final long serialVersionUID = 6676247325789548938L;
+	private int requestSize;
+	
+	public RequestEntityTooLargeException(int requestSize) {
 		super(413, "Request Entity Too Large");
 	}
+	
+	public RequestEntityTooLargeException() {
+		this(0);
+	}
+
+	@Override
+	public String toString() {
+		return "RequestEntityTooLargeException [requestSize=" + requestSize
+				+ "], + " + super.toString();
+	}
+		
 }
