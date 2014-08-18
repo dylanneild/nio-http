340545a181851eaa783fab67085a982167595bbd
simplepanda <dylan@codeandstrings.com>
2014-08-18 10:30:21 -0400

    Added support for GMT only date return (per spec)

--- nio-http/com/codeandstrings/niohttp/data/DateUtils.java
+++ nio-http/com/codeandstrings/niohttp/data/DateUtils.java
@@ -3,15 +3,33 @@ package com.codeandstrings.niohttp.data;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class DateUtils {
 
     public static SimpleDateFormat RFC_822_DATE_FORMAT
             = new SimpleDateFormat("EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z", Locale.US);
 
+    public static SimpleDateFormat RFC_822_ZONE_LESS_FORMAT
+            = new SimpleDateFormat("EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss", Locale.US);
 
     public static final String getRfc822DateString(Date date) {
         return DateUtils.RFC_822_DATE_FORMAT.format(date);
+    }
+
+    public static final String getRfc822DateStringGMT(Date date) {
+
+        String pattern = RFC_822_ZONE_LESS_FORMAT.toPattern();
+        SimpleDateFormat format = new SimpleDateFormat(pattern, Locale.US);
+        StringBuilder r = new StringBuilder();
+
+        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        r.append(format.format(date));
+        r.append(" GMT");
+
+        return r.toString();
+
     }
 
 }
