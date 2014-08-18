4647f93a0d0850a2e13fc68849dee0440ed0cd4f
simplepanda <dylan@codeandstrings.com>
2014-08-17 21:11:10 -0400

    Forked debuging for my development environment.

--- nio-http/com/codeandstrings/niohttp/debug/Debug.java
+++ nio-http/com/codeandstrings/niohttp/debug/Debug.java
@@ -1,12 +1,22 @@
 package com.codeandstrings.niohttp.debug;
 
 import com.codeandstrings.niohttp.Server;
+import com.codeandstrings.niohttp.data.Parameters;
 
 public class Debug {
 
 	public static void main(String args[]) {
 
 		Server server = new Server();
+        boolean isLocal = false;
+
+        if (System.getProperty("os.name") != null && System.getProperty("os.name").equalsIgnoreCase("mac os x")) {
+            isLocal = true;
+        }
+
+        if (!isLocal) {
+            server.setParameters(new Parameters(80));
+        }
 
         try {
             server.addRequestHandler("/pictures/.*", DebugPictureHandler.class);
