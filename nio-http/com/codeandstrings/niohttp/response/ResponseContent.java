d33c28a57edf2a9105aa997fea123ae022bdfc92
simplepanda <dylan@codeandstrings.com>
2014-08-24 21:13:35 -0400

    Removed ResponseContentHeader - not needed in queue based design.

--- nio-http/com/codeandstrings/niohttp/response/ResponseContent.java
+++ nio-http/com/codeandstrings/niohttp/response/ResponseContent.java
@@ -1,14 +1,20 @@
 package com.codeandstrings.niohttp.response;
 
+import java.util.Arrays;
+
 public class ResponseContent implements ResponseMessage {
 
-    private ResponseContentHeader bufferHeader;
+    private long sessionId;
+    private long requestId;
+    private boolean lastBufferForRequest;
 	private byte[] buffer;
     private byte[] headerBuffer;
     private byte[] footerBuffer;
 
     public ResponseContent(long sessionId, long requestId, byte[] buffer, boolean lastBufferForRequest) {
-        this.bufferHeader = new ResponseContentHeader(sessionId, requestId, lastBufferForRequest);
+        this.sessionId = sessionId;
+        this.requestId = requestId;
+        this.lastBufferForRequest = lastBufferForRequest;
         this.buffer = buffer;
     }
 
@@ -36,6 +42,30 @@ public class ResponseContent implements ResponseMessage {
         this.buffer = buffer;
     }
 
+    public long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(long sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public long getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(long requestId) {
+        this.requestId = requestId;
+    }
+
+    public boolean isLastBufferForRequest() {
+        return lastBufferForRequest;
+    }
+
+    public void setLastBufferForRequest(boolean lastBufferForRequest) {
+        this.lastBufferForRequest = lastBufferForRequest;
+    }
+
     public int getTotalBufferSize() {
         int h = this.headerBuffer == null ? 0 : this.headerBuffer.length;
         int f = this.footerBuffer == null ? 0 : this.footerBuffer.length;
@@ -44,44 +74,42 @@ public class ResponseContent implements ResponseMessage {
     }
 
     @Override
-    public long getSessionId() {
-        return bufferHeader.getSessionId();
-    }
-
-    public boolean isLastBufferForRequest() {
-        return bufferHeader.isLastBufferForRequest();
-    }
-
-    @Override
-    public long getRequestId() {
-        return bufferHeader.getRequestId();
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof ResponseContent)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
         ResponseContent that = (ResponseContent) o;
 
-        if (buffer != null ? !buffer.equals(that.buffer) : that.buffer != null) return false;
-        if (bufferHeader != null ? !bufferHeader.equals(that.bufferHeader) : that.bufferHeader != null) return false;
+        if (lastBufferForRequest != that.lastBufferForRequest) return false;
+        if (requestId != that.requestId) return false;
+        if (sessionId != that.sessionId) return false;
+        if (!Arrays.equals(buffer, that.buffer)) return false;
+        if (!Arrays.equals(footerBuffer, that.footerBuffer)) return false;
+        if (!Arrays.equals(headerBuffer, that.headerBuffer)) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        int result = bufferHeader != null ? bufferHeader.hashCode() : 0;
-        result = 31 * result + (buffer != null ? buffer.hashCode() : 0);
+        int result = (int) (sessionId ^ (sessionId >>> 32));
+        result = 31 * result + (int) (requestId ^ (requestId >>> 32));
+        result = 31 * result + (lastBufferForRequest ? 1 : 0);
+        result = 31 * result + (buffer != null ? Arrays.hashCode(buffer) : 0);
+        result = 31 * result + (headerBuffer != null ? Arrays.hashCode(headerBuffer) : 0);
+        result = 31 * result + (footerBuffer != null ? Arrays.hashCode(footerBuffer) : 0);
         return result;
     }
 
     @Override
     public String toString() {
-        return "BufferContainer{" +
-                "bufferHeader=" + bufferHeader +
-                ", buffer=" + buffer +
+        return "ResponseContent{" +
+                "sessionId=" + sessionId +
+                ", requestId=" + requestId +
+                ", lastBufferForRequest=" + lastBufferForRequest +
+                ", buffer=" + Arrays.toString(buffer) +
+                ", headerBuffer=" + Arrays.toString(headerBuffer) +
+                ", footerBuffer=" + Arrays.toString(footerBuffer) +
                 '}';
     }
 }
